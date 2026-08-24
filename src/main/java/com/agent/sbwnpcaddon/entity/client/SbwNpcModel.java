3a4c07fbefbdf0de5ac0b27be1a36e6bfb8ad081
ShinigamiKiri <hauhaudepzai@gmail.com>
2026-08-24 18:58:06 +0700

    Fix model visual backwards rotation

--- src/main/java/com/agent/sbwnpcaddon/entity/client/SbwNpcModel.java
+++ src/main/java/com/agent/sbwnpcaddon/entity/client/SbwNpcModel.java
@@ -25,4 +25,13 @@ public class SbwNpcModel extends GeoModel<SbwNpcEntity> {
     public ResourceLocation getAnimationResource(SbwNpcEntity object) {
         return new ResourceLocation("sbw_npc_addon", "animations/" + modelName + ".animation.json");
     }
+
+    @Override
+    public void setCustomAnimations(SbwNpcEntity animatable, long instanceId, software.bernie.geckolib.core.animation.AnimationState<SbwNpcEntity> animationState) {
+        super.setCustomAnimations(animatable, instanceId, animationState);
+        software.bernie.geckolib.cache.object.GeoBone rootBone = this.getAnimationProcessor().getBone("root");
+        if (rootBone != null) {
+            rootBone.setRotY(rootBone.getRotY() + (float)Math.PI);
+        }
+    }
 }
